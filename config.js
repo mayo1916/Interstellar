65e1a0844b9a8ad47dbd7a0e3dda1d3dd3deaa15
mayo1916 <127997715+mayo1916@users.noreply.github.com>
2024-08-29 13:49:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: { mayo1916
     // You can add multiple users by doing username: 'password'.
-    interstellar: "WASD=n0tMe",
+    interstellar: "password",'WASD=n0tMe'
   },
 }
 export default config
