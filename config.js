893c228990978606a739639c38ba5a6ff9796755
mayo1916 <127997715+mayo1916@users.noreply.github.com>
2024-08-29 13:51:04 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: true, // Set to true if you want to enable password protection.
+  challenge: false, // Set to true if you want to enable password protection.
   users: { mayo1916
     // You can add multiple users by doing username: 'password'.
     interstellar: "password",'WASD=n0tMe'
